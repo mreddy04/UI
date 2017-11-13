2360312bfe3f7abf07366bbf350084249e6fde39
mreddy04 <mrmallikarjunareddy@gmail.com>
2017-11-14 00:56:29 +0530

    Added new files

--- js/script.js
+++ js/script.js
@@ -12,21 +12,6 @@
       }
   });
 
-  // app.config(function($routeProvider) {
-  //   $routeProvider
-  //   .when("/", {
-  //       templateUrl : "main.htm"
-  //   })
-  //   .when("/red", {
-  //       templateUrl : "red.htm"
-  //   })
-  //   .when("/green", {
-  //       templateUrl : "green.htm"
-  //   })
-  //   .when("/blue", {
-  //       templateUrl : "blue.htm"
-  //   });
-  // });
   
   app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/home');
