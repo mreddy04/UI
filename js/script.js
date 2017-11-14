4a6c238af4375ff55ba1ca75755d9b538ca2619d
mreddy04 <mrmallikarjunareddy@gmail.com>
2017-11-15 03:11:28 +0530

    updated files

--- js/script.js
+++ js/script.js
@@ -61,6 +61,41 @@
   });
   
   app.controller('HomeController', function($scope, $stateParams, $state) {
+      $scope.chartOptions = {
+        title: {
+            text: 'Temperature data'
+        },
+        xAxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 
+                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        },
+
+        series: [{
+            data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+        }]
+    };
+
+    $scope.pieData = [{
+            name: "Microsoft Internet Explorer",
+            y: 56.33
+        }, {
+            name: "Chrome",
+            y: 24.03,
+            sliced: true,
+            selected: true
+        }, {
+            name: "Firefox",
+            y: 10.38
+        }, {
+            name: "Safari",
+            y: 4.77
+        }, {
+            name: "Opera",
+            y: 0.91
+        }, {
+            name: "Proprietary or Undetectable",
+            y: 0.2
+    }]
 
   });
   
@@ -87,7 +122,51 @@
   app.controller('ThankyouController', function($scope, $stateParams, $state, saveContact) {
     $scope.contactPerson = saveContact.getContact();
   });
-
+  app.directive('hcChart', function () {
+    return {
+      restrict: 'E',
+      template: '<div></div>',
+      scope: {
+          options: '='
+      },
+      link: function (scope, element) {
+          Highcharts.chart(element[0], scope.options);
+      }
+    };
+  });
+  app..directive('hcPieChart', function () {
+    return {
+      restrict: 'E',
+      template: '<div></div>',
+      scope: {
+          title: '@',
+          data: '='
+      },
+      link: function (scope, element) {
+        Highcharts.chart(element[0], {
+          chart: {
+              type: 'pie'
+          },
+          title: {
+              text: scope.title
+          },
+          plotOptions: {
+            pie: {
+              allowPointSelect: true,
+              cursor: 'pointer',
+              dataLabels: {
+                enabled: true,
+                format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+              }
+            }
+          },
+          series: [{
+              data: scope.data
+          }]
+        });
+      }
+    };
+  });
 
   app.factory('LoginService', function() {
     var admin = 'a';
