29d10dde53894f95c2a82de65a0a6cd110e350aa
mreddy04 <mrmallikarjunareddy@gmail.com>
2017-11-14 22:28:56 +0530

    added new files

--- js/script.js
+++ js/script.js
@@ -60,13 +60,17 @@
   });
   
   app.controller('ContactController', function($scope, $stateParams, $state) {
-    function myMap() {
-      var mapProp= {
-          center:new google.maps.LatLng(51.508742,-0.120850),
-          zoom:5,
-      };
-      var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-    }
+    // function initMap() {
+    //   var uluru = {lat: -25.363, lng: 131.044};
+    //   var map = new google.maps.Map(document.getElementById('map'), {
+    //     zoom: 4,
+    //     center: uluru
+    //   });
+    //   var marker = new google.maps.Marker({
+    //     position: uluru,
+    //     map: map
+    //   });
+    // }
   });
 
 
